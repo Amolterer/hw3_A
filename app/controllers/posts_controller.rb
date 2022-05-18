c81262eb540ccf5e69599cb6ba3095f634ecc38a
Amolterer <alexander.molterer@kellogg.northwestern.edu>
2022-05-18 15:40:26 +0000

    step5

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,10 +14,10 @@ class PostsController < ApplicationController
         @post = Post.new
         @post["title"] = params["post"]["title"]
         @post["description"] = params["post"]["description"]
-        @post["posted_on"] = params["post"]["posted_on"]
+        @post["posted_on"] = params["post"]["date"]
         @post["place_id"] = params["post"]["place_id"]
         @post.save
-        redirect_to "/places/#{@post["place_id"]}"
+        redirect_to "/places/"
       end
 
 end
